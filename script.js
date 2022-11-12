54c71a08dc0e4ea033e34516a1a74f76787c57ba
linneszyx <itssuryanshcooldude346@gmail.com>
2022-11-12 13:32:22 +0530

    final ver

--- script.js
+++ script.js
@@ -1,41 +1,124 @@
-const imgcontainer = document.getElementById('img-container');
+/* Getting the elements from the HTML file. */
+const piccontain = document.getElementById('img-container');
 const loader = document.getElementById('loader');
-let photosArray = [];
 
-const count = 10;
-const apikey = '_E0VveRwBuWURH7KzSzsfF61lUvS6Njm7xEpjmI7AHc';
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${_E0VveRwBuWURH7KzSzsfF61lUvS6Njm7xEpjmI7AHc}&count=${10}`;
+/* A variable that is set to false. */
+let ready = false;
+/* Setting the variable `imagesLoaded` to 0. */
+let imagesLoaded = 0;
+/* Setting the variable `totalImages` to 0. */
+let totalImages = 0;
+/* Creating an empty array. */
+let photosArray = [];
+/* Setting the variable `count` to 10. */
+let count = 10;
+/* Setting the variable `apiKey` to the value `d7gSfkiywZ8NsS1NQInOCgjqojSph_0Uf845i1sRpv4`. */
+const apiKey = 'd7gSfkiywZ8NsS1NQInOCgjqojSph_0Uf845i1sRpv4';
+
+/**
+ * If the number of images loaded is equal to the total number of images, then the game is ready to
+ * start.
+ */
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+    }
+
+}
+
+
+/**
+ * This function takes an element and an object of attributes and sets the attributes on the element.
+ * @param element - The element you want to add the attributes to.
+ * @param attributes - an object containing the attributes to set.
+ */
 function setAttributes(element, attributes) {
-    for (const key in element) {
-        setAttributes(key, attributes[key]);
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
     }
 }
 
+
+/**
+ * The function loops through the photosArray and creates an anchor element for each photo. 
+ * 
+ * The anchor element is given an href attribute that links to the photo's html page. 
+ * 
+ * The anchor element is also given a target attribute that opens the link in a new tab. 
+ * 
+ * The anchor element is then given an image element. 
+ * 
+ * The image element is given a src attribute that links to the photo's regular size image. 
+ * 
+ * The image element is also given an alt attribute that contains the photo's alt description. 
+ * 
+ * The image element is also given a title attribute that contains the photo's description. 
+ * 
+ * The image element is then given an event listener that calls the imageLoaded function when the image
+ * loads. 
+ * 
+ * The image element is then appended to the anchor element. 
+ * 
+ * The anchor element is then
+ */
 function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+
     photosArray.forEach((photo) => {
         const item = document.createElement('a');
         setAttributes(item, {
             href: photo.links.html,
-            target: '_blank',
+            target: '_blank'
         });
-        const img = document.createElement('img');
-        setAttributes(img, {
+
+        const image = document.createElement('img');
+
+        setAttributes(image, {
             src: photo.urls.regular,
             alt: photo.alt_description,
-            title: photo.alt_description,
+            title: photo.description
         });
-        item.appendChild(img);
-        imgcontainer.appendChild(item);
-    });
+        image.addEventListener('load', imageLoaded);
+
+
+        item.appendChild(image);
+        piccontain.appendChild(item);
+    })
 }
+
+/**
+ * The getPhotos function is an asynchronous function that fetches the photos from the Unsplash API and
+ * displays them on the page.
+ */
 async function getPhotos() {
     try {
+        const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
         const response = await fetch(apiUrl);
         photosArray = await response.json();
         displayPhotos();
+        loader.hidden = true;
+        count = 20;
+
     } catch (error) {
 
     }
 }
 
+/* Listening for the scroll event. When the scroll event occurs, it checks to see if the
+window's scroll position plus the window's inner height is greater than or equal to the document's
+body's offset height minus 1000. If it is, then it sets the variable `ready` to false and calls the
+function `getPhotos`. */
+window.addEventListener('scroll', () => {
+
+    if ((window.scrollY + window.innerHeight >= document.body.offsetHeight - 1000) && ready) {
+
+        ready = false;
+        getPhotos();
+    }
+});
+
+
+/* Calling the function `getPhotos`. */
 getPhotos();
